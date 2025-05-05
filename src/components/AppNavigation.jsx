1fda209a6d2c0e32ee35224c59f4679848fbe932
mbulimatshe <mbulimatshe11@gmail.com>
2025-05-05 12:01:23 -0500

    Added AppNavigation component

---
+++ src/components/AppNavigation.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AppNavigation = () => {
+  return (
+    <nav className="p-4 bg-gray-100 border-b">
+      <h1 className="text-xl font-bold">SimplyTrade</h1>
+    </nav>
+  );
+};
+
+export default AppNavigation;
